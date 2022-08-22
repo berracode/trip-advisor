ad7fac632177ff2f8c95b13da5e4af91c7848045
Juan Guillermo Hernandez Alarcon <jghernande@bch.bancodechile.cl>
2022-08-22 12:48:12 -0500

    First commit

---
+++ src/screens/Restaurants.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export function Restaurants(){
+    return(
+        <View>
+            <Text>Screen de restaurants</Text>
+        </View>
+    )
+}
